cf062dad18bbcd228976b9327ecb81b3c61dc25d
Gary Marriott <ramriot@users.noreply.github.com>
2015-01-16 15:35:42 -0500

    Adding signature vlaidation

--- TweetNaCl.php
+++ TweetNaCl.php
@@ -872,5 +872,39 @@ class TweetNaCl {
 		$this->crypto_box_beforenm($k, $y, $x);
 		return $this->crypto_box_open_afternm($m, $c, $d, $n, $k);
 	}
+	
+	/* Crypto signature */
+	function crypto_sign_open($m, $sm, $n, $pk) {
+  var i, mlen;
+  var t = new Uint8Array(32), h = new Uint8Array(64);
+  var p = [gf(), gf(), gf(), gf()],
+      q = [gf(), gf(), gf(), gf()];
+
+  mlen = -1;
+  if (n < 64) return -1;
+
+  if (unpackneg(q, pk)) return -1;
+
+  for (i = 0; i < n; i++) m[i] = sm[i];
+  for (i = 0; i < 32; i++) m[i+32] = pk[i];
+  crypto_hash(h, m, n);
+  reduce(h);
+  scalarmult(p, q, h);
+
+  scalarbase(q, sm.subarray(32));
+  add(p, q);
+  pack(t, p);
+
+  n -= 64;
+  if (crypto_verify_32(sm, 0, t, 0)) {
+    for (i = 0; i < n; i++) m[i] = 0;
+    return -1;
+  }
+
+  for (i = 0; i < n; i++) m[i] = sm[i + 64];
+  mlen = n;
+  return mlen;
+}
+
 
 }
